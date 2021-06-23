b3adcfcd3d6d35b2302ec4a22f4a0cb38e673522
KatyaZav <Party2015ru@mail.ru>
2021-06-23 17:30:12 +0500

    Final Gameplay

--- Second Game Snake one/Form1.cs
+++ Second Game Snake one/Form1.cs
@@ -21,61 +21,74 @@ namespace Second_Game_Snake_one
             Down=3
         }
 
-        int Length;
-        Point[] mas = new Point[64];
+        int Length =1;
+        Point[] mas; 
         Direction MoveTurn;
-        Point head = new Point(150,150);
         Point apple;
-        bool AppleEat = true;
-
+        
         public Form1()
         {
+            mas = new Point[64];
             InitializeComponent();
             MoveTurn = 0;
-            Length = 1;
-            mas[0] = head;
+            mas[0] = new Point(150,150);
+            Random rnd = new Random();
+            apple.X = rnd.Next(0, 7) * 50;
+            apple.Y = rnd.Next(0, 7) * 50;
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-            if (AppleEat)
-            {
-                Random rnd = new Random();
-                var x = rnd.Next(0, 7)*50;
-                var y = rnd.Next(0, 7)*50;
-                apple.X = x;
-                apple.Y = y;
-                AppleEat = false;
-            }
+        {                             
+            var colorApple = new SolidBrush(Color.Red);
+            e.Graphics.FillEllipse(colorApple, apple.X, apple.Y, 50, 50);
 
+            for (var i = Length; i > 0; i--)
+                mas[i] = mas[i-1];
 
             if (MoveTurn == 0)
-                head.X+=50;
+                mas[0].X += 50;
             if (MoveTurn == Direction.Left)
-                head.X -= 50;
+                mas[0].X -= 50;
             if (MoveTurn == Direction.Up)
-                head.Y -= 50;
+                mas[0].Y -= 50;
             if (MoveTurn == Direction.Down)
-                head.Y += 50;
+                mas[0].Y += 50;
 
-            if (head.Y<0 || head.X <0 || head.Y>=400 || head.X >= 400)
+            var colorSnake = new SolidBrush(Color.Black);
+            for (var i = 0; i < Length; i++)
             {
-                head.X = 150;
-                head.Y = 150;                
+                e.Graphics.FillEllipse(colorSnake, mas[i].X, mas[i].Y, 50, 50);
             }
 
-            if (!AppleEat && apple.X == head.X && head.Y == apple.Y)
+            if (mas[0].Y < 0 || mas[0].X < 0 || mas[0].Y >= 350 || mas[0].X >= 350)
             {
-                AppleEat = true;
+                GameOver();
+            }
+
+            for (var i = 1; i<Length;i++)
+            {
+                if (mas[0] == mas[i])
+                    GameOver();
+            }
+
+            if (apple.X == mas[0].X && mas[0].Y == apple.Y)
+            {
                 Length++;
+                Random rnd = new Random();
+                apple.X = rnd.Next(0, 7) * 50;
+                apple.Y = rnd.Next(0, 7) * 50;
+                //if (Length > 10)
+                //    timer1.Interval = 300;
+                //if (Length > 20)
+                //    timer1.Interval = 150;
             }
-            else
-            {
-                var colorApple = new SolidBrush(Color.Red);
-                e.Graphics.FillEllipse(colorApple, apple.X, apple.Y, 50, 50);
-            }
-            var colorSnake = new SolidBrush(Color.Blue);
-            e.Graphics.FillRectangle(colorSnake,head.X,head.Y,50,50);
+        }
+
+        private void GameOver()
+        {
+            mas[0].X = 150;
+            mas[0].Y = 150;
+            Length = 1;
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
